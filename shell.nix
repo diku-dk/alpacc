6556adc3e33d657448a6f8129c741ccf1db86f30
due <williamhenrichdue@gmail.com>
2023-05-27 04:06:14 +0200

    Maybe I were missing the git package.

--- shell.nix
+++ shell.nix
@@ -2,12 +2,13 @@ with import <nixpkgs> { };
 pkgs.mkShell {
   buildInputs = [
     python3
+    futhark
+    git
     python3Packages.mypy
     python3Packages.black
     python3Packages.futhark-ffi
     python3Packages.numpy
     haskell.compiler.ghc927
     cabal-install
-    futhark
   ];
 }
