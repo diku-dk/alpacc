df78a674fffbbe5624c1b3abb4afe6e74d0aca3c
due <williamhenrichdue@gmail.com>
2023-05-27 04:02:40 +0200

    Almost done with the Futhark test suite. I should make it so it fails more gracefully.

--- shell.nix
+++ shell.nix
@@ -4,6 +4,8 @@ pkgs.mkShell {
     python3
     python3Packages.mypy
     python3Packages.black
+    python3Packages.futhark-ffi
+    python3Packages.numpy
     haskell.compiler.ghc927
     cabal-install
     futhark
