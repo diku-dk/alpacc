5c52770be3e33d442d12eebff2730a87a18a247f
due <williamhenrichdue@gmail.com>
2023-05-27 12:23:59 +0200

    This should fix the problem.

--- shell.nix
+++ shell.nix
@@ -8,6 +8,7 @@ pkgs.mkShell {
     python310Packages.black
     python310Packages.pip
     python310Packages.numpy
+    python310Packages.virtualenv
     haskell.compiler.ghc927
     cabal-install
   ];
