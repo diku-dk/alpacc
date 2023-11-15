87bef74f9fccabcb42ff7d6c8493174c4bd54ae6
Due <williamhenrichdue@gmail.com>
2023-11-15 16:01:09 +0100

    I think something like this is what that will be needed.

--- src/Alpacc/Lexer/ParallelLexing.hs
+++ src/Alpacc/Lexer/ParallelLexing.hs
@@ -12,72 +12,124 @@ import Data.Functor.Identity
 import Data.Bifunctor
 -- import Data.List qualified as List
 
-data Comp s = Comp
-  { maxEdge :: Integer,
-    compositionMap :: Map (Integer, Integer) Integer,
-    connectionMap :: Map s (Set s),
-    edgeMap :: Map (s, s) (Set Integer)
+data Comp t s k = Comp
+  { maxTrans :: t,
+    compositionMap :: Map (t, t) t,
+    endsMap :: Map t (Set s),
+    connectedMap :: Map t (Set t),
+    tokenMap :: Map t (Set k)
   }
   deriving (Eq, Show)
 
-type CompState s = State (Comp s) ()
+type CompState t s k a = State (Comp t s k) a
 
-transitions' :: (IsState s, IsTransition t) => DFA t s -> Map (s, t) s
+addComposition :: (IsTransition t, Enum t, Ord k) => t -> t -> CompState t s k t
+addComposition t t' = do
+  comp <- get
+  token_map <- gets tokenMap
+  max_trans <- gets maxTrans
+  composition_map <- gets compositionMap
+  ends_map <- gets endsMap
+
+  let new_max_trans = succ max_trans
+
+  let t_token_set = Map.lookup t token_map
+  let t_token_set' = Map.lookup t' token_map
+  let new_token_map =
+        flip (Map.insert new_max_trans) token_map
+        $ fromMaybe Set.empty
+        $ liftM2 Set.intersection t_token_set t_token_set'
+
+  let new_ends = fromMaybe Set.empty $ Map.lookup t' ends_map
+  let new_ends_map = Map.insert t' new_ends ends_map
+  
+  let new_composition_map = Map.insert (t, t') new_max_trans composition_map
+  
+  let new_comp =
+        comp
+        { maxTrans = new_max_trans
+        , compositionMap = new_composition_map
+        , tokenMap = new_token_map
+        , endsMap = new_ends_map
+        }
+
+  let maybe_t = Map.lookup (t, t') composition_map
+  put $ case maybe_t of
+    Just _ -> comp
+    Nothing -> new_comp
+
+  return $ fromMaybe new_max_trans maybe_t
+
+endsLookup :: (IsTransition t, IsState s) => t -> CompState t s k (Set s)
+endsLookup t = do
+  ends_map <- gets endsMap
+  return $ fromMaybe Set.empty $ Map.lookup t ends_map
+
+connectedLookup :: (IsTransition t, IsState s) => t -> CompState t s k (Set t)
+connectedLookup t = do
+  connected_map <- gets connectedMap
+  return $ fromMaybe Set.empty $ Map.lookup t connected_map
+
+transitions' :: (IsTransition t, IsState s) => DFA t s -> Map (s, t) s
 transitions' =
   Map.mapKeys (second runIdentity)
   . fmap runIdentity
   . transitions
 
-complete :: (IsState s) => DFA Integer s -> Map (s, Integer) s
+complete :: (IsTransition t, IsState s) => DFA t s -> Map (s, t) s
 complete dfa = trans
   where
-    -- alpha = alphabet dfa
     trans = transitions' dfa
 
-initConnectionMap :: (IsState s) => DFA Integer s -> Map s (Set s)
-initConnectionMap dfa =
+initEnds :: (IsTransition t, IsState s) => DFA t s -> Map t (Set s)
+initEnds dfa =
   Map.unions
-  $ auxiliary <$> _states
+  $ auxiliary
+  <$> _alphabet
   where
     _alphabet = Set.toList $ alphabet dfa
     _states = Set.toList $ states dfa
     _transitions = transitions' dfa
 
-    auxiliary s =
-      Map.singleton s
+    auxiliary t =
+      Map.singleton t
       $ Set.fromList
-      $ mapMaybe ((`Map.lookup` _transitions) . (s, )) _alphabet
+      $ mapMaybe ((`Map.lookup` _transitions) . (, t)) _states
 
-initEdgeMap :: (IsState s) => DFA Integer s -> Map (s, s) (Set Integer)
-initEdgeMap dfa =
-  Map.unions
-  $ auxiliary <$> _states
+initConnected :: (IsTransition t, IsState s) => DFA t s -> Map t (Set t)
+initConnected dfa = Map.fromList $ auxiliary <$> _alphabet
   where
     _alphabet = Set.toList $ alphabet dfa
     _states = Set.toList $ states dfa
     _transitions = transitions' dfa
 
-    auxiliary s =
-      Map.unionsWith Set.union
-      $ mapMaybe (edgeLookup s) _alphabet
+    auxiliary t =
+      (t, )
+      . Set.unions
+      $ transitionsLookup
+      <$> mapMaybe ((`Map.lookup` _transitions) . (, t)) _states
 
-    edgeLookup s t = do
-      s' <- Map.lookup (s, t) _transitions
-      return $ Map.singleton (s, s') $ Set.singleton t
+    transitionLookup s t =
+      if (s, t) `Map.member` _transitions
+      then Just t
+      else Nothing
+    
+    transitionsLookup s =
+      Set.fromList
+      $ mapMaybe (transitionLookup s) _alphabet
 
-initComp :: (IsState s) => DFA Integer s -> Comp s
+initComp :: (IsTransition t, IsState s) => DFA t s -> Comp t s k
 initComp dfa =
   Comp
-  { maxEdge = max_edge
+  { maxTrans = max_trans
   , compositionMap = composition_map
-  , connectionMap = connection_map
-  , edgeMap = edge_map
+  , connectedMap = connected_map
+  , endsMap = ends_map
+  , tokenMap = Map.empty
   }
   where
-    max_edge = maximum $ alphabet dfa
+    max_trans = maximum $ alphabet dfa
     composition_map = Map.empty
-    connection_map = initConnectionMap dfa
-    edge_map = initEdgeMap dfa
+    connected_map = initConnected dfa
+    ends_map = initEnds dfa
 
--- newEdges :: (IsState s) => s -> CompState s
--- newEdges s = 
