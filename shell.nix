f9723a5b0a301af645c8464bf22b2e01828dff88
Due <williamhenrichdue@gmail.com>
2024-05-19 17:00:07 +0200

    Fixed enviroment.

--- shell.nix
+++ shell.nix
@@ -8,37 +8,36 @@ let pkg =
       compiler.developPackage {
         root = ./.;
         modifier = drv:
-          pkgs.haskell.lib.addBuildTools drv (with pkgs.haskellPackages;
-            [ cabal-install
-              ghcid
-              haskell-language-server
-            ]);
+          pkgs.haskell.lib.addBuildTools drv
+            ((with pkgs.haskellPackages;
+              [ cabal-install
+                ghcid
+                haskell-language-server
+              ]) ++
+            (with pkgs;
+              [ python3
+                git gitRepo gnupg autoconf curl
+                procps gnumake util-linux m4 gperf unzip
+                cudatoolkit linuxPackages.nvidia_x11
+                libGLU libGL
+                xorg.libXi xorg.libXmu freeglut
+                xorg.libXext xorg.libX11 xorg.libXv xorg.libXrandr zlib 
+                ncurses5 binutils
+                opencl-headers
+                clinfo
+                gcc11
+                gmp
+                ispc
+                ocl-icd
+                rustc
+                cargo
+              ]) ++
+            (with unstable;
+              [ futhark
+                mkjson
+              ]));
       };
 in pkg.overrideAttrs (attrs: {
-  buildInputs =
-    attrs.buildInputs ++
-    (with pkgs;
-      [ python3
-        git gitRepo gnupg autoconf curl
-        procps gnumake util-linux m4 gperf unzip
-        cudatoolkit linuxPackages.nvidia_x11
-        libGLU libGL
-        xorg.libXi xorg.libXmu freeglut
-        xorg.libXext xorg.libX11 xorg.libXv xorg.libXrandr zlib 
-        ncurses5 stdenv.cc binutils
-        opencl-headers
-        clinfo
-        gcc
-        gmp
-        ispc
-        ocl-icd
-        rustc
-        cargo
-      ]) ++
-    (with unstable;
-      [ futhark
-        mkjson
-      ]);
   shellHook = attrs.shellHook + ''
     export CUDA_PATH=${pkgs.cudatoolkit}
     # export LD_LIBRARY_PATH=${pkgs.linuxPackages.nvidia_x11}/lib:${pkgs.ncurses5}/lib
