4f1f722f54c4cab295b10a7a8df7a447d2fcc9b1
Due <williamhenrichdue@gmail.com>
2026-02-02 22:59:35 +0100

    Nevermind gonna have to rewrite a bit.

--- src/Alpacc/Generator/Cuda/Parser.hs
+++ src/Alpacc/Generator/Cuda/Parser.hs
@@ -19,10 +19,18 @@ generateParser terminal_type parser =
   (Text.strip . Text.pack)
     [i|
 using token_t = #{cudafy terminal_type};
-const size_t Q = #{q}
-const size_t K = #{k}
+using production_t = #{cudafy production_type};
+using bracket_t = #{cudafy bracket_type};
+const size_t Q = #{q};
+const size_t K = #{k};
+const token_t empty_terminal = #{empty_terminal};
+const size_t number_of_productions = #{number_of_productions} 
 |]
     <> cudaParser
   where
+    production_type = productionType parser
+    bracket_type = bracketType parser
     q = lookback parser
     k = lookahead parser
+    empty_terminal = emptyTerminal parser
+    number_of_productions = numberOfProductions parser
